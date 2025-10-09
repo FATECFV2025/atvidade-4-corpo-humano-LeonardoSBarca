a7af48b9080c03022d228079709fb5e56d0414b6
LeonardoSBarca <leonardo.barca@fatec.sp.gov.br>
2025-10-08 23:10:45 -0300

    ATV 1 POO
    Trabalho do prof francisco

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,29 +2,38 @@ package main.java;
 import java.util.Scanner;
 
 public class App {
-        //Comentário
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
 
-    public static void funcao_teste (){
-        //Biblioteca para entrada de dados
-        Scanner ler = new Scanner(System.in); 
+        System.out.println("=== CRIAÇÃO DO OBJETO CORPO HUMANO ===");
+        System.out.print("Digite a massa inicial (kg): ");
+        float massa = sc.nextFloat();
+        System.out.print("Digite o volume inicial (m³): ");
+        float volume = sc.nextFloat();
+        System.out.print("Digite a densidade inicial (kg/m³): ");
+        float densidade = sc.nextFloat();
+        System.out.print("Digite a altura inicial (m): ");
+        double altura = sc.nextDouble();
 
-        //variável local a
-        String v2;
-        int v1;
+        CorpoHumano c1 = new CorpoHumano(massa, volume, densidade, altura);
 
-        System.out.println("Comando de Saida de Dados");
-        
-        System.out.printf("Informe uma palavra: ");
-        v2 = ler.next();
+        System.out.println("=== VALORES INICIAIS ===");
+        System.out.println("Massa: " + c1.getMassa());
+        System.out.println("Volume: " + c1.getVolume());
+        System.out.println("Densidade: " + c1.getDensidade());
+        System.out.println("Altura: " + c1.getAltura());
+        System.out.println("IMC: " + c1.calcularIMC());
 
-        System.out.printf("Informe um número: ");
-        v1 = ler.nextInt();
+        while(true){
+            System.out.println("\nDigite nova massa (kg): ");
+            c1.setMassa(sc.nextFloat());
+            System.out.println("Digite nova altura (m): ");
+            c1.setAltura(sc.nextDouble());
 
-        System.out.printf(v2+" "+v1);
-    }
-    public static void main(String[] args) throws Exception {
-        funcao_teste();
-
-
+            System.out.println("=== NOVOS VALORES ===");
+            System.out.println("Nova massa: " + c1.getMassa());
+            System.out.println("Nova altura: " + c1.getAltura());
+            System.out.println("Novo IMC: " + c1.calcularIMC());
+        }
     }
 }
