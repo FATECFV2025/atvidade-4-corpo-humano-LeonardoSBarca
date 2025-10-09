15d8fb7e83fb2128a1e95d7ace618450ef64e78a
LeonardoSBarca <leonardo.barca@fatec.sp.gov.br>
2025-10-08 23:15:11 -0300

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -17,11 +17,14 @@ public class App {
 
         CorpoHumano c1 = new CorpoHumano(massa, volume, densidade, altura);
 
+
+
         System.out.println("=== VALORES INICIAIS ===");
         System.out.println("Massa: " + c1.getMassa());
         System.out.println("Volume: " + c1.getVolume());
         System.out.println("Densidade: " + c1.getDensidade());
         System.out.println("Altura: " + c1.getAltura());
+
         System.out.println("IMC: " + c1.calcularIMC());
 
         while(true){
@@ -37,3 +40,19 @@ public class App {
         }
     }
 }
+//questão 1
+//quando eu coloquei a linha c1.massa = "2", deu erro porque o valor estava entre aspas, e o java entende que "2" é String e não numero
+
+//CONCLUSÃO: o jeito certo de escrever é sem as aspas para que o java entenda como tipo int
+
+
+//questao 2
+//quando troquei private por public float massa eu consegui mudar o valor da massa direto no programa, por exemplo: c1.massa = 80;
+//isso aconteceu porque com o publico atributo pode ser acessado e alterado de qualquer lugar do codigo
+
+//CONCLUSAO: posso acessar e altrear sempre que eu quiser
+
+//questao 3
+//quanto eu mudei o método setVolume para private, não é mais possível chamar ele no programa principal, por exemplo com c1.setVolume(10)
+//isso aconteceu porque o metodo private só pode ser usado dentro da própria classe
+//CONCLUSAO: o main nao poderia mais mexer no volumne
